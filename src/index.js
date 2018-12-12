e17bf25b6f953bfb682e8461587b0c3e0f7b3d89
愚道 <tingzhao.ytz@antfin.com>
2018-12-12 17:54:48 +0800

    feat: support copy _mock.js to mock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 // ref:
 // - https://umijs.org/plugin/develop.html
+import { join } from 'path';
 
-export default function (api, options) {
-
-  // Example: output the webpack config
-  api.chainWebpackConfig(config => {
-    console.log(config.toString());
+export default function (api) {
+  const { paths } = api;
+  api._modifyBlockTarget((target, { sourceName, blockPath }) => {
+    if (sourceName === '_mock.js') {
+      // /test/t/_mock.js -> test-t.js
+      const mockFileName = blockPath.replace(/^\//, '').replace(/\//g, '');
+      return join(paths.cwd, 'mock', `${mockFileName}.js`);
+    }
+    return target;
   });
 }
