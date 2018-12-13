0ca59c88993bf7942f02c4c11f1bca58a75506b8
愚道 <tingzhao.ytz@antfin.com>
2018-12-13 15:51:11 +0800

    support move service.js to services and replace umi-request to util/request

--- example/.umirc.js
+++ example/.umirc.js
@@ -3,5 +3,9 @@ import { join } from 'path';
 export default {
   plugins: [
     join(__dirname, '..', require('../package').main || 'index.js'),
+    ['umi-plugin-react', {
+      dva: true,
+      antd: true,
+    }],
   ],
 }
